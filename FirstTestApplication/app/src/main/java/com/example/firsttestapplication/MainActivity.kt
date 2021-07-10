169950d61501ec8a28ff8cadd237d1448aa15dd0
王好球先辈 <wu.wa.a@hotmail.com>
2021-07-11 00:53:39 +0800

    Update Roll Dice
    Follow the Kotlin training

--- FirstTestApplication/app/src/main/java/com/example/firsttestapplication/MainActivity.kt
+++ FirstTestApplication/app/src/main/java/com/example/firsttestapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.firsttestapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener {
+            val textView: TextView = findViewById(R.id.textView)
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            val dice = Dice(6)
+            textView.text = dice.roll().toString()
+        }
+    }
+
+    class Dice(private val numSides: Int) {
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
     }
 }
