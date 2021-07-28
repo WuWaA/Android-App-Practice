31ac96398295d65af665279a9223dbe0ced5a2b8
王好球先辈 <wu.wa.a@hotmail.com>
2021-07-29 01:30:18 +0800

    More practices
    Add more activity and others

--- FirstTestApplication/app/src/main/java/com/example/firsttestapplication/MainActivity.kt
+++ FirstTestApplication/app/src/main/java/com/example/firsttestapplication/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.firsttestapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,11 +16,27 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.rollButton)
         rollButton.setOnClickListener {
             val textView: TextView = findViewById(R.id.textView)
+            val diceImage: ImageView = findViewById(R.id.diceImage)
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
-            val dice = Dice(6)
-            textView.text = dice.roll().toString()
+            val dice = Dice(20)
+            val diceRoll = dice.roll()
+            textView.text = diceRoll.toString()
+            val imageID = when (diceRoll) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            diceImage.setImageResource(imageID)
         }
+    }
+
+    fun switchActivity(view: View) {
+        val intent = Intent(this, LayoutActivity::class.java)
+        startActivity(intent)
     }
 
     class Dice(private val numSides: Int) {
